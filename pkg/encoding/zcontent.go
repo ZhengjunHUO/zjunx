a5642570b40163a7a74448ff393cf89980c7faa3
ZhengjunHUO <firelouiszj@hotmail.com>
2021-07-01 15:33:11 +0200

    define Content type

--- pkg/encoding/zcontent.go
+++ pkg/encoding/zcontent.go
@@ -2,6 +2,20 @@ package encoding
 
 type ZContentType uint64
 
-type ZContent interface {
+const metadataSize uint8 = 16
 
+type ZContent interface {}
+
+type Content struct {
+	Type	ZContentType
+	Len	uint64
+	Data	[]byte
+}
+
+func ContentInit(t ZContentType, d []byte) ZContent {
+	return &Content {
+		Type: t,
+		Len: uint64(len(d)),
+		Data: d,
+	}
 }
