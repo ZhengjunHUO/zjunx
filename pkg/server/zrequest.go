645f647a6388b1cbd2dc9b05841d39dc7d0af227
ZhengjunHUO <firelouiszj@hotmail.com>
2021-07-02 22:06:13 +0200

    implement Request struct

--- pkg/server/zrequest.go
+++ pkg/server/zrequest.go
@@ -8,3 +8,18 @@ type ZRequest interface {
 	GetContentType() encoding.ZContentType
 }
 
+type Request struct {
+	conn	ZConnection
+	cont	*encoding.Content
+}
+
+func ReqInit(conn ZConnection, cont *encoding.Content) ZRequest {
+	return &Request{
+		conn: conn,
+		cont: cont,
+	}
+}
+
+func (r *Request) GetContentType() encoding.ZContentType {
+	return r.cont.Type
+}
